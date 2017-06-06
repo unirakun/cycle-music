67dad892f24e53b63ea5a3f1ad1d8cb8cc4e9ef4
Fabien JUIF <fabien.juif@zenika.com>
2017-06-06 13:56:41 +0200

    :sparkles: Livecoding 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { run } from '@cycle/run'
 import { makeDOMDriver } from '@cycle/dom'
+import isolate from '@cycle/isolate'
 import list from './list'
 import Character from './character'
 
 const CHARACTER_NAMES = ['zora', 'goron']
 
 const main = (sources) => {
-  const characters = CHARACTER_NAMES.map(name => Character({ ...sources, name }))
+  const characters = CHARACTER_NAMES.map(name => isolate(Character, name)({ ...sources, name }))
 
   const sinks = {
     ...list(characters)(sources),
