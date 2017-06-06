e613617d2314bf51e97809778d874ef57144e5d6
Fabien JUIF <fabien.juif@zenika.com>
2017-06-06 13:53:03 +0200

    :sparkles: Livecoding 1

--- src/character.js
+++ src/character.js
@@ -1,6 +1,19 @@
-// import { img } from '@cycle/dom'
+import { img } from '@cycle/dom'
 
-export default (/* sources */) => {
-  const sinks = {}
+export default (sources) => {
+  const { DOM$, name } = sources
+
+  const actions$ = DOM$.select('.character').events('click')
+  const model$ = actions$.mapTo(true).startWith(false)
+  const vdom$ = model$.map(animate => (
+    img(
+      `.character${animate ? '.animate' : ''}`,
+      { props: { src: `/svg/characters/${name}.svg` } },
+    )
+  ))
+
+  const sinks = {
+    DOM$: vdom$,
+  }
   return sinks
 }
